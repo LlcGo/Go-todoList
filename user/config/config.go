1fade01ca44dc09ed2557c7b5362514fd8416f99
lc <1641767724@qq.com>
2025-06-08 17:17:08 +0800

    viper reader yml config.yml

---
+++ user/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"os"
+)
+
+func InitDB() {
+	workDir, _ := os.Getwd() // 工作目录路径
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+}
